0af6e8d9bb09dd7c8e3c3a46e64ae9021aca1e5f
Carlos <carloswelliton7@gmail.com>
2025-11-01 21:19:54 -0400

    First commit

---
+++ src/test/java/com/example/SpringBoot_ErrorHandling/SpringBootErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_ErrorHandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootErrorHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
